dd1df482a6fa1c799a9a1665bdab100b5e66bbd2
vitavann <vitavann@gmail.com>
2019-09-03 15:44:13 -0700

    adds logic to getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,24 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className, node) {
+  var res = [];
+  if(node === undefined) {
+    node = document.body;
+  }
+
+  if (node.classList) {
+    if (node.classList.contains(className)) {
+      res.push(node);
+    }
+  }
+
+  if (node.hasChildNodes()) {
+    var children = node.childNodes;
+    for (var i = 0; i < children.length; i++) {
+      res = res.concat(getElementsByClassName(className, children[i]));
+    }
+  }
+
+  return res;
 };
