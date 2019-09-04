366fd3d85e923f332fafe6b6891ce5e8d1ece845
vitavann <vitavann@gmail.com>
2019-09-03 20:14:13 -0700

    adds initial logic for parseJSON

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,48 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  // your code goes here
+  var firstChar = json[0];
+
+  if (firstChar === '[') {
+    var results = [];
+    var insideArray = json.slice(1, json.length - 1);
+    if (insideArray.length > 0) {
+      var items = insideArray.split(/, |,/);
+      items.forEach(function(item) {
+        results = results.concat(parseJSON(item));
+      });
+    }
+    return results;
+  }
+
+  if (firstChar === '{') {
+    var results = {};
+    var insideObj = json.slice(1, json.length - 1);
+    if (insideObj.length > 0) {
+      var items = insideObj.split(/, |,/);
+      items.forEach(function(item) {
+        var keyVal = item.split(/: |:/);
+        var key = parseJSON(keyVal[0]);
+        var val = parseJSON(keyVal[1]);
+        results[key] = val;
+      });
+    }
+    return results;
+  }
+
+  if (firstChar === '"') {
+    return json.slice(1, json.length - 1);
+  }
+
+  if (json === 'null') {
+    return null;
+  }
+  if (json === 'true') {
+    return true;
+  }
+  if (json === 'false') {
+    return false;
+  }
+
+  return parseFloat(json);
 };
